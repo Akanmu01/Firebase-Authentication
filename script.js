693f671034be483e4d0c96a25e762f07c1bdba3a
Akanmu Akinkunmi <akinjossy1298@gmail.com>
2019-11-21 12:39:13 +0000

    sctipt.js

---
+++ script.js
@@ -0,0 +1,20 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.4.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBpSpFWOD3BCOJxnawyVoUQAthljz-iP8E",
+    authDomain: "my-first-project-141c6.firebaseapp.com",
+    databaseURL: "https://my-first-project-141c6.firebaseio.com",
+    projectId: "my-first-project-141c6",
+    storageBucket: "my-first-project-141c6.appspot.com",
+    messagingSenderId: "314131406478",
+    appId: "1:314131406478:web:6dfff647078451a60173df"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
